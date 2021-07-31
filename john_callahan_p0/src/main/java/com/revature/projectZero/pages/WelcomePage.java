345d1de381eb4960ddfcdf7124642d3b01e510c0
heraldOfMechanus <jccallahan11@gmail.com>
2021-07-31 10:54:43 -0500

    Made a few changes to the file. Attempting to inject the BufferedReader dependancy into the WelcomePage class.

--- john_callahan_p0/src/main/java/com/revature/projectZero/pages/WelcomePage.java
+++ john_callahan_p0/src/main/java/com/revature/projectZero/pages/WelcomePage.java
@@ -1,22 +1,27 @@
 package com.revature.projectZero.pages;
 
-public class WelcomePage {
+import java.io.BufferedReader;
+
+public class WelcomePage extends Page {
 
     //Singleton Method implementation; Welcome Screen may not be created multiple times.
     private static final WelcomePage welcome = new WelcomePage();
 
-    private WelcomePage() {
-
+    private WelcomePage(BufferedReader consoleReader) {
+        super(consoleReader);
     }
 
     public static WelcomePage getWelcome() { return welcome; }
 
+    @Override
     public void render() {
         //Prompt the user for a selection of one of these three things.
         System.out.println("Please make a selection: \n" +
                 "\n1) Login" +
                 "\n2) Register a new Student" +
-                "\n3) Exit Application");
-    }
+                "\n3) Exit Application" +
+                "\n>");
 
+
+    }
 }
