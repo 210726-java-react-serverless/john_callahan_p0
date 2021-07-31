528cd0dc42dde890745ccd079415cbd18bfd2ede
heraldOfMechanus <jccallahan11@gmail.com>
2021-07-31 14:10:07 -0500

    Page class gives router to extensions; created Student plain ol' Java object.

--- john_callahan_p0/src/main/java/com/revature/projectZero/pages/Page.java
+++ john_callahan_p0/src/main/java/com/revature/projectZero/pages/Page.java
@@ -1,17 +1,21 @@
 package com.revature.projectZero.pages;
 
+import com.revature.projectZero.util.PageRouter;
 import java.io.BufferedReader;
+
 
 public abstract class Page {
 
     protected String name;
     protected String route;
     protected BufferedReader reader;
+    protected PageRouter router;
 
-    public Page(String name, String route, BufferedReader reader) {
+    public Page(String name, String route, BufferedReader reader, PageRouter router) {
         this.name = name;
         this.route = route;
         this.reader = reader;
+        this.router = router;
     }
 
     public String getName() {
---
+++ john_callahan_p0/src/main/java/com/revature/projectZero/pojos/Student.java
@@ -0,0 +1,87 @@
+package com.revature.projectZero.pojos;
+
+import java.util.Objects;
+
+public class Student {
+
+    private int studentID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+
+    public Student(String firstName, String lastName, String email, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Student(String firstName, String lastName, String email, String username, String password, int id) {
+        this(firstName,lastName,email,username,password);
+        this.studentID = id;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentID == student.studentID && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(email, student.email) && Objects.equals(username, student.username) && Objects.equals(password, student.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentID, firstName, lastName, email, username, password);
+    }
+}
