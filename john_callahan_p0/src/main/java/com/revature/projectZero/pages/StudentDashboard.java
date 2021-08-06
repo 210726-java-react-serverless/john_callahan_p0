d43856b5542d9cc29ec2f16bff8859f20f7bceae
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-06 16:26:33 -0500

    Removed unnecessary return; statement.

--- john_callahan_p0/src/main/java/com/revature/projectZero/pages/StudentDashboard.java
+++ john_callahan_p0/src/main/java/com/revature/projectZero/pages/StudentDashboard.java
@@ -50,7 +50,6 @@ public class StudentDashboard extends Page{
             case "logout":
                 router.navigate("/welcome");
                 checker.logout();
-                return;
         }
     }
 }
