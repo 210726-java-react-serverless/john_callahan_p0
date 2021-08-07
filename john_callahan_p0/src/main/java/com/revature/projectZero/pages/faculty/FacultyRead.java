caff6e99cfaed43dab09eee9d24bdc9c307897af
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-07 18:23:46 -0500

    Created the bare-bones faculty pages.

---
+++ john_callahan_p0/src/main/java/com/revature/projectZero/pages/faculty/FacultyRead.java
@@ -0,0 +1,18 @@
+package com.revature.projectZero.pages.faculty;
+
+import com.revature.projectZero.pages.Page;
+import com.revature.projectZero.util.PageRouter;
+
+import java.io.BufferedReader;
+
+public class FacultyRead extends Page {
+
+    public FacultyRead(String name, String route, BufferedReader reader, PageRouter router) {
+        super(name, route, reader, router);
+    }
+
+    @Override
+    public void render() throws Exception {
+
+    }
+}
---
+++ john_callahan_p0/src/main/java/com/revature/projectZero/pages/faculty/FacultyUpdate.java
@@ -0,0 +1,18 @@
+package com.revature.projectZero.pages.faculty;
+
+import com.revature.projectZero.pages.Page;
+import com.revature.projectZero.util.PageRouter;
+
+import java.io.BufferedReader;
+
+public class FacultyUpdate extends Page {
+
+    public FacultyUpdate(String name, String route, BufferedReader reader, PageRouter router) {
+        super(name, route, reader, router);
+    }
+
+    @Override
+    public void render() throws Exception {
+
+    }
+}
