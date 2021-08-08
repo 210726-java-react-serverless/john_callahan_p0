d311b0816b76c8eb16513907e1356c1af81fec26
heraldOfMechanus <jccallahan11@gmail.com>
2021-08-07 20:22:44 -0500

    Finished the Faculty view screen.

--- john_callahan_p0/src/main/java/com/revature/projectZero/pages/faculty/FacultyRead.java
+++ john_callahan_p0/src/main/java/com/revature/projectZero/pages/faculty/FacultyRead.java
@@ -1,10 +1,12 @@
 package com.revature.projectZero.pages.faculty;
 
 import com.revature.projectZero.pages.Page;
+import com.revature.projectZero.pojos.Course;
 import com.revature.projectZero.service.ValidationService;
 import com.revature.projectZero.util.PageRouter;
 
 import java.io.BufferedReader;
+import java.util.List;
 
 public class FacultyRead extends Page {
 
@@ -18,6 +20,45 @@ public class FacultyRead extends Page {
     @Override
     public void render() throws Exception {
 
+        List<Course> courses = checker.getTeacherClasses();
 
+        if(courses != null) {
+            for (Course cours : courses) {
+                System.out.println("\nName: " + cours.getName()
+                        + "\nID/Call-Sign: " + cours.getId()
+                        + "\nDescription: " + cours.getDesc()
+                        + "\nTeacher: " + cours.getTeacher()
+                        + "\nOpen: " + cours.isOpen());
+            }
+        } else {
+            System.out.println("You have no courses!");
+        }
+
+        System.out.print("\nWhat would you like to do?"
+                    + "\n1) Delete a class"
+                    + "\n2) Update a class"
+                    + "\n3) Go back to the dashboard"
+                    + "> ");
+        String input = reader.readLine();
+
+        switch(input) {
+            case "1":
+            case "delete":
+            case "Delete":
+                System.out.println("Sending you to Course Deletion services...");
+                router.navigate("/facDelete");
+                break;
+            case "2":
+            case "update":
+            case "Update":
+                System.out.println("Sending you to Course Update services...");
+                router.navigate("/facUpdate");
+            case "3":
+            case "back":
+            case "Back":
+            default:
+                System.out.println("Sending you back to the Dashboard...");
+                router.navigate("/f_dashboard");
+        }
     }
 }
